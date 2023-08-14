3ef740f31a587e8a55a82e9f6491ae3adcd8595d
lsc0x80 <ruqwe0@gmail.com>
2023-08-14 09:45:08 +0000

    Java/Main.java: Xor code moved to function

--- Java/Main.java
+++ Java/Main.java
@@ -3,16 +3,7 @@ package Java;
 import java.util.Base64;
 
 class Main {
-    public static void main(String[] args) {
-        String key = args[0];
-        String text = args[1];
-        Base64.Encoder encoder = Base64.getEncoder();
-        Base64.Decoder decoder = Base64.getDecoder();
-
-        System.out.println("key: "+key);
-        System.out.println("text: "+text);
-        //char[] textc = text.toCharArray();
-        //char[] keyc = key.toCharArray();
+    public static byte[] Xor(String text, String key) {
         int k = 0;
         int kl = key.length()-1;
         byte[] cryptotext = new byte[text.length()];
@@ -21,6 +12,18 @@ class Main {
             if (k == kl) k = 0;
             else k++;
         }
-        System.out.println(encoder.encodeToString(cryptotext));
+        return cryptotext;
+    }
+
+    public static void main(String[] args) {
+        String key = args[0];
+        String text = args[1];
+        Base64.Encoder encoder = Base64.getEncoder();
+        Base64.Decoder decoder = Base64.getDecoder();
+
+        System.out.println("key: "+key);
+        System.out.println("text: "+text);
+        
+        System.out.println(encoder.encodeToString(Xor(text, key)));
     }
 }
