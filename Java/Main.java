1cf6448988b1c467868e4edf213f74e706848887
lsc0x80 <ruqwe0@gmail.com>
2023-08-14 11:26:14 +0000

    Java/Main.java: Added decode mode

--- Java/Main.java
+++ Java/Main.java
@@ -18,12 +18,16 @@ class Main {
     public static void main(String[] args) {
         String key = args[0];
         String text = args[1];
+        // 0 - Xor
+        // 1 - UnXor
+        String mode = (args.length > 2) ? args[2] : "0";
         Base64.Encoder encoder = Base64.getEncoder();
         Base64.Decoder decoder = Base64.getDecoder();
 
         System.out.println("key: "+key);
         System.out.println("text: "+text);
-        
-        System.out.println(encoder.encodeToString(Xor(text, key)));
+
+        if (mode == "0") System.out.println(encoder.encodeToString(Xor(text, key)));
+        else System.out.println(new String(Xor(new String(decoder.decode(text)), key)));
     }
 }
