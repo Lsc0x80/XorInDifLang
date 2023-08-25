1e4717c032bc2df55d4b709f93047c5282460802
lsc0x80 <ruqwe0@gmail.com>
2023-08-25 08:49:23 +0000

    Rust: code readability optimization

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,5 +1,5 @@
 use base64::{encode, decode};
-use std::io::{self, Write};
+use std::{io::{self, Write}, process::exit};
 use clearscreen;
 
 fn xor_with_key(input: &String, key: &String) -> String {
@@ -26,33 +26,28 @@ fn main() {
         print!(">>> ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut chs).expect("Failed to read line");
+
+        if chs.to_lowercase().chars().nth(0).unwrap() == 'q' {exit(0);}
+
+        print!("data: ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut data)
+            .expect("Failed to read line");
+
+        print!("key: ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut key)
+            .expect("Failed to read line");
+
         if chs.to_lowercase().chars().nth(0).unwrap() == 'e' {
-            print!("data: ");
-            io::stdout().flush().unwrap();
-            io::stdin()
-                .read_line(&mut data)
-                .expect("Failed to read line");
-            print!("key: ");
-            io::stdout().flush().unwrap();
-            io::stdin()
-                .read_line(&mut key)
-                .expect("Failed to read line");
             data = data.replace("\n", "");
             key = key.replace("\n", "");
             let xored = xor_with_key(&data, &key);
             let encoded = encode(xored);
             println!("{}", encoded);
         } else if chs.to_lowercase().chars().nth(0).unwrap() == 'd' {
-            print!("data: ");
-            io::stdout().flush().unwrap();
-            io::stdin()
-                .read_line(&mut data)
-                .expect("Failed to read line");
-            print!("key: ");
-            io::stdout().flush().unwrap();
-            io::stdin()
-                .read_line(&mut key)
-                .expect("Failed to read line");
             data = data.replace("\n", "");
             key = key.replace("\n", "");
             let decoded = String::from_utf8(decode(data.clone()).unwrap()).unwrap();
